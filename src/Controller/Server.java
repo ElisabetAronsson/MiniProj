b3d3d11f28dc7fe028da51647eea479f30a23d0f
Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>
2023-05-24 17:39:04 +0200

    Seperate output stream for each client

--- src/Controller/Server.java
+++ src/Controller/Server.java
@@ -24,7 +24,6 @@ public class Server {
      */
     private ServerSocket serverSocket;
     private Socket clientSocket;
-    private ObjectOutputStream oos;
     private UserProcedures userProcedures;
     private ProductProcedures productProcedures;
 
@@ -41,11 +40,12 @@ public class Server {
         while (true) {
             //Accept a clients connection.
             clientSocket = serverSocket.accept();
-            //Create output stream to send objects to the client.
-            oos = new ObjectOutputStream(clientSocket.getOutputStream());
             System.out.println("Client connected: " + clientSocket.getInetAddress());
             new Thread(() -> {
                 try {
+                    //Create output stream to send objects to the client.
+                    ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+
                     //Create an input stream to read objects sent from the client.
                     ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
                     while(true){
@@ -55,7 +55,7 @@ public class Server {
                         if(object instanceof Product){
                             //Add product to database here.
                             Product product = (Product) object;
-                            handleProductFromClient(product);
+                            handleProductFromClient(product, oos);
                         }
                         else if(object instanceof User){
                             //Log in or register user here.
@@ -63,11 +63,11 @@ public class Server {
                             //Check if user is registered.
                             if(user.isRegistered()){
                                 //Login user.
-                                handleUserLoginFromClient(user);
+                                handleUserLoginFromClient(user, oos);
                             }
                             else {
                                 //Register user.
-                                handleUserRegisterFromClient(user);
+                                handleUserRegisterFromClient(user, oos);
                             }
                         }
                         else if (object instanceof Request){
@@ -76,32 +76,32 @@ public class Server {
                                 handleAddCartFromClient(request);
                             }
                             else if(request.getRequestType().equals("declineRequest")){
-                                handleDeclineBuyReqFromClient(request);
+                                handleDeclineBuyReqFromClient(request, oos);
                             }
                             else if(request.getRequestType().equals("acceptRequest")){
-                                handleAcceptBuyReqFromClient(request);
+                                handleAcceptBuyReqFromClient(request, oos);
                             }
                             else if(request.getRequestType().equals("searchByType")){
-                                sendSearchByTypeToClient(request);
+                                sendSearchByTypeToClient(request, oos);
                             }
                             else if(request.getRequestType().equals("searchByPrice")){
-                                sendSearchByPriceToClient(request);
+                                sendSearchByPriceToClient(request, oos);
                             }
                             else if(request.getRequestType().equals("searchByCondition")){
-                                sendSearchByConditionToClient(request);
+                                sendSearchByConditionToClient(request, oos);
                             }
                             else if(request.getRequestType().equals("searchByDate")){
-                                sendSearchByDateToClient(request, request.getUserId());
+                                sendSearchByDateToClient(request, request.getUserId(), oos);
                             }
                             else if(request.getRequestType().equals("showAllProducts")){
-                                getAllProductsFromDatabase();
+                                getAllProductsFromDatabase(oos);
                             }
                             else if(request.getRequestType().equals("viewCart")){
-                                sendCartItemsToClient(request.getUserId());
+                                sendCartItemsToClient(request.getUserId(), oos);
                             }
                             else if(request.getRequestType().equals("removeFromCart")){
                                 if(productProcedures.removeFromCart(request.getUserId(), request.getProduct_id())) {
-                                    sendCartItemsToClient(request.getUserId());
+                                    sendCartItemsToClient(request.getUserId(), oos);
                                 }
                             }
                             else if(request.getRequestType().equals("requestItemFromCart")){
@@ -110,19 +110,19 @@ public class Server {
                         }
                         else if (object instanceof String){
                             if (object == "marketplace") {
-                                getAllProductsFromDatabase();
+                                getAllProductsFromDatabase(oos);
                             }
                         } else if(object instanceof Integer){
-                            sendClientUsersProducts((int)object);
+                            sendClientUsersProducts((int)object, oos);
                         }
                         else if(object instanceof ServerRequest){
                             ServerRequest serverRequest = (ServerRequest) object;
                             if(serverRequest.getRequestType().equals("getOrderHistory")){
-                                sendClientOrderHistory(serverRequest.getUserID());
+                                sendClientOrderHistory(serverRequest.getUserID(), oos);
                             }
 
                             if(serverRequest.getRequestType().equals("getRequests")){
-                                sendClientRequests(serverRequest.getUserID());
+                                sendClientRequests(serverRequest.getUserID(), oos);
                             }
                         }
                     }
@@ -137,22 +137,22 @@ public class Server {
         }
     }
 
-    private void sendCartItemsToClient(int userId) throws SQLException, IOException {
+    private void sendCartItemsToClient(int userId, ObjectOutputStream oos) throws SQLException, IOException {
         oos.writeObject(productProcedures.getUserShoppingcart(userId));
         oos.flush();
     }
 
-    private void sendClientUsersProducts(int userId) throws IOException, SQLException {
+    private void sendClientUsersProducts(int userId, ObjectOutputStream oos) throws IOException, SQLException {
         oos.writeObject(productProcedures.getUsersProducts(userId));
         oos.flush();
     }
 
-    private void sendClientOrderHistory(int userId) throws IOException, SQLException {
+    private void sendClientOrderHistory(int userId, ObjectOutputStream oos) throws IOException, SQLException {
         oos.writeObject(productProcedures.getOrderHistory(userId));
         oos.flush();
     }
 
-    private void sendClientRequests(int userId) throws IOException, SQLException {
+    private void sendClientRequests(int userId, ObjectOutputStream oos) throws IOException, SQLException {
         oos.writeObject(productProcedures.getBuyReqs(userId));
         oos.flush();
     }
@@ -169,7 +169,7 @@ public class Server {
      * Sends the products by type to the client.
      * @param request Includes data that was sent from the client.
      */
-    public void sendSearchByTypeToClient(Request request) throws IOException {
+    public void sendSearchByTypeToClient(Request request, ObjectOutputStream oos) throws IOException {
         Hashtable results = productProcedures.getProductsByTitle(request.getParam());
         oos.writeObject(results);
         oos.flush();
@@ -179,25 +179,25 @@ public class Server {
      * Sends the products by price to the client.
      * @param request Includes data that was sent from the client.
      */
-    public void sendSearchByPriceToClient(Request request) throws IOException {
+    public void sendSearchByPriceToClient(Request request, ObjectOutputStream oos) throws IOException {
         Hashtable results = productProcedures.getProductsByPrice(request.getParam());
         oos.writeObject(results);
         oos.flush();
     }
-    public void sendSearchByDateToClient(Request request, int userID) throws IOException {
+    public void sendSearchByDateToClient(Request request, int userID, ObjectOutputStream oos) throws IOException {
         String startDate, endDate;
         String temp = request.getParam();
         startDate = temp.substring(0, temp.indexOf("|"));
         endDate = temp.substring(temp.indexOf("|") + 1, temp.length());
         Hashtable results = productProcedures.searchByDate(startDate, endDate, userID);
-        sendHashtableToClient(results);
+        sendHashtableToClient(results, oos);
     }
 
     /**
      * Sends the products by condition to the client.
      * @param request Includes data that was sent from the client.
      */
-    public void sendSearchByConditionToClient(Request request) throws IOException {
+    public void sendSearchByConditionToClient(Request request, ObjectOutputStream oos) throws IOException {
         Hashtable results = productProcedures.getProductsByCondition(request.getParam());
         oos.writeObject(results);
         oos.flush();
@@ -207,39 +207,39 @@ public class Server {
      * Handles the buy request from the client.
      * @param request The request object holding data
      */
-    public void handleAcceptBuyReqFromClient(Request request) throws SQLException, IOException {
+    public void handleAcceptBuyReqFromClient(Request request, ObjectOutputStream oos) throws SQLException, IOException {
         productProcedures.purchaseProd(request.getProduct_id(), request.getProductName(), request.getBuyer_id());
         //Update the client with the new request table from the DB.
-        sendClientRequests(request.getUserId());
+        sendClientRequests(request.getUserId(), oos);
         //Update the client with the new product table from the DB after deleting the accepted product.
-        getAllProductsFromDatabase();
+        getAllProductsFromDatabase(oos);
     }
 
     /**
      * This function handles the decline request from the client.
      * @param request The request object holding data
      */
-    public void handleDeclineBuyReqFromClient(Request request) throws SQLException, IOException {
+    public void handleDeclineBuyReqFromClient(Request request, ObjectOutputStream oos) throws SQLException, IOException {
         productProcedures.declinePurchaseRequest(request.getProduct_id());
-        sendClientRequests(request.getUserId());
+        sendClientRequests(request.getUserId(), oos);
     }
 
     /**
      * Handles messages from the client.
      * The function decides what to do based on the message.
      */
-    public void handleProductFromClient(Product product) throws IOException {
+    public void handleProductFromClient(Product product, ObjectOutputStream oos) throws IOException {
         productProcedures.registerProdForSale(product);
         //Get all the products from the database to update the GUI.
-        getAllProductsFromDatabase();
+        getAllProductsFromDatabase(oos);
     }
 
     /**
      * Handles the user registration.
      * @param user The user object that was sent from the client.
      */
-    public void handleUserRegisterFromClient(User user) throws IOException {
-        sendStringMessageToClient("The user has been registered.");
+    public void handleUserRegisterFromClient(User user, ObjectOutputStream oos) throws IOException {
+        sendStringMessageToClient("The user has been registered.", oos);
         userProcedures.createUser(user.getUsername(), user.getPassword(), user.getDateOfBirth(), user.getEmail());
     }
 
@@ -247,18 +247,18 @@ public class Server {
      * Handles the user login.
      * @param user The user object that was sent from the client.
      */
-    public void handleUserLoginFromClient(User user) throws IOException {
+    public void handleUserLoginFromClient(User user, ObjectOutputStream oos) throws IOException {
         int userId = userProcedures.signInUser(user.getUsername(), user.getPassword());
         //If higher than 0, login was successfull.
         if(userId > 0){
-            sendStringMessageToClient("loginSuccess");
+            sendStringMessageToClient("loginSuccess", oos);
             //Send the user id that was returned from the database to the client.
-            sendUserIdToClient(userId);
+            sendUserIdToClient(userId, oos);
             //Get all the products from the database and send to the client to show on the GUI.
-            getAllProductsFromDatabase();
+            getAllProductsFromDatabase(oos);
         }
         else {
-            sendStringMessageToClient("loginFailed");
+            sendStringMessageToClient("loginFailed", oos);
         }
     }
 
@@ -266,10 +266,10 @@ public class Server {
      * This function gets all the products from the database.
      * @throws IOException
      */
-    public void getAllProductsFromDatabase() throws IOException {
+    public void getAllProductsFromDatabase(ObjectOutputStream oos) throws IOException {
         Hashtable hashtable = productProcedures.getAllProducts();
         //Send the DefaultTableModel holding the data to the client.
-        sendHashtableToClient(hashtable);
+        sendHashtableToClient(hashtable, oos);
     }
 
     /**
@@ -277,7 +277,7 @@ public class Server {
      * @param hashtable The table model with the data that will be displayed in a JTable plus its type.
      * @throws IOException
      */
-    public void sendHashtableToClient(Hashtable<String, DefaultTableModel> hashtable) throws IOException {
+    public void sendHashtableToClient(Hashtable<String, DefaultTableModel> hashtable, ObjectOutputStream oos) throws IOException {
         oos.writeObject(hashtable);
         oos.flush();
     }
@@ -287,7 +287,7 @@ public class Server {
      * @param message
      * @throws IOException
      */
-    public void sendStringMessageToClient(String message) throws IOException {
+    public void sendStringMessageToClient(String message, ObjectOutputStream oos) throws IOException {
         oos.writeObject(message);
         oos.flush();
     }
@@ -297,7 +297,7 @@ public class Server {
      * @param userId The users id.
      * @throws IOException
      */
-    public void sendUserIdToClient(int userId) throws IOException {
+    public void sendUserIdToClient(int userId, ObjectOutputStream oos) throws IOException {
         oos.writeObject(userId);
         oos.flush();
     }
